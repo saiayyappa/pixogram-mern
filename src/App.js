d47f5dbff73679a373c74486ebb9e1ff2e29a538
saiayyappa <saiayyappaor@gmail..com>
2019-11-12 18:45:44 +0530

    added Settings Component with child routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import _ from "underscore";
+
 function ProfileCard() {
   return (
     <div className="card">
       <img
+        alt=""
         src="https://dummyimage.com/300x300/999/fff"
         className="card-img-top"
       />
@@ -21,10 +23,132 @@ function MediaCard() {
   return (
     <div className="col-4 mb-4">
       <img
+        alt=""
         src="https://dummyimage.com/300x300/999/fff"
         className="card-img-top"
       />
     </div>
+  );
+}
+
+function Update() {
+  return (
+    <>
+      <div className="shadow-lg p-4">
+        <BrowserRouter>
+          <ul className="nav nav-pills mb-4">
+            <li className="nav-item">
+              <Link
+                className="nav-link text-secondary"
+                to="/settings/update/username"
+              >
+                Update Username
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link text-secondary"
+                to="/settings/update/password"
+              >
+                Update Password
+              </Link>
+            </li>
+          </ul>
+          <Route path="/settings/update/username" component={Notfound} />
+          <Route path="/settings/update/password" component={Notfound} />
+        </BrowserRouter>
+      </div>
+    </>
+  );
+}
+
+function Followers() {
+  return (
+    <>
+      <div className="row">
+        {_.range(3).map(() => (
+          <MediaCard />
+        ))}
+      </div>
+    </>
+  );
+}
+
+function Followings() {
+  return (
+    <>
+      <div className="row">
+        {_.range(2).map(() => (
+          <MediaCard />
+        ))}
+      </div>
+    </>
+  );
+}
+
+function Blocked() {
+  return (
+    <>
+      <div className="row">
+        {_.range(4).map(() => (
+          <MediaCard />
+        ))}
+      </div>
+    </>
+  );
+}
+
+function Settings() {
+  return (
+    <>
+      <div className="container">
+        <HeadPanel />
+        <div className="row">
+          <div className="col">
+            <BrowserRouter>
+              <ul className="nav nav-pills mb-4">
+                <li className="nav-item">
+                  <Link
+                    className="nav-link text-secondary"
+                    to="/settings/update"
+                  >
+                    Update User Details
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link text-secondary"
+                    to="/settings/followers"
+                  >
+                    Followers
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link text-secondary"
+                    to="/settings/followings"
+                  >
+                    Followings
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link text-secondary"
+                    to="/settings/blocked"
+                  >
+                    Blocked users
+                  </Link>
+                </li>
+              </ul>
+              <Route path="/settings/update" component={Update} />
+              <Route path="/settings/followers" component={Followers} />
+              <Route path="/settings/followings" component={Followings} />
+              <Route path="/settings/blocked" component={Blocked} />
+            </BrowserRouter>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
@@ -39,48 +163,28 @@ function UploadFile() {
           </div>
           <div className="col-9">
             <h5>Upload Files</h5>
-            <div className="row">
-              <form>
-                <div class="row mt-2">
-                  <div class="col">
-                    <div class="text-center">
-                      <img width="100%" height="350px" />
-                      <img class="img-fluid" height="250px" />
-                      <div class="image-text-centered lead">
-                        Drop image here
-                      </div>
-                    </div>
+            <img alt="" className="mb-4" width="100%" height="350px" />
+            <form>
+              <div class="card-text d-flex mb-2">
+                <div class="custom-file">
+                  <input
+                    type="file"
+                    class="custom-file-input"
+                    id="validatedCustomFile"
+                    required
+                  />
+                  <label class="custom-file-label" for="validatedCustomFile">
+                    Choose file...
+                  </label>
+                  <div class="invalid-feedback">
+                    Example invalid custom file feedback
                   </div>
                 </div>
-                <div class="card-text d-flex mb-2">
-                  <div class="custom-file">
-                    <input
-                      type="file"
-                      class="custom-file-input"
-                      id="validatedCustomFile"
-                      required
-                    />
-                    <label class="custom-file-label" for="validatedCustomFile">
-                      Choose file...
-                    </label>
-                    <div class="invalid-feedback">
-                      Example invalid custom file feedback
-                    </div>
-                  </div>
-                  <button type="submit" class="btn btn-outline-dark ml-1">
-                    Upload
-                  </button>
-                </div>
-                <div class="row">
-                  <div class="col d-flex justify-content-center lead">
-                    <button class="badge badge-dark m-1">Sepia</button>
-                    <button class="badge badge-dark m-1">Gray</button>
-                    <button class="badge badge-dark m-1">Brightness</button>
-                    <button class="badge badge-dark m-1">Contrast</button>
-                  </div>
-                </div>
-              </form>
-            </div>
+                <button type="submit" class="btn btn-outline-dark ml-2">
+                  Upload
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
@@ -99,7 +203,7 @@ function MediaPage() {
         <div className="col-9">
           <h5>Media Page</h5>
           <div className="row">
-            {_.range(7).map(() => (
+            {_.range(8).map(() => (
               <MediaCard />
             ))}
           </div>
@@ -226,12 +330,21 @@ function RegisterPage() {
   );
 }
 
+function Notfound() {
+  return <h1>Page not found</h1>;
+}
+
 function App() {
   return (
     <BrowserRouter>
-      <Route exact path="/" component={SignInPage} />
-      <Route path="/register" component={RegisterPage} />
-      <Route path="/media" component={MediaPage} />
+      <Switch>
+        <Route exact path="/" component={SignInPage} />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/media" component={MediaPage} />
+        <Route path="/upload" component={UploadFile} />
+        <Route path="/settings" component={Settings} />
+        <Route component={Notfound} />
+      </Switch>
     </BrowserRouter>
   );
 }
